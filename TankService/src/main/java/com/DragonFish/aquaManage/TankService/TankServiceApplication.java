d5accd6180d5ccb4367a75faf94d3c5eae18f2d8
Riju Mondal <mailrijumondal@gmail.com>
2024-09-04 00:59:17 +0530

    Basic Microservice, UserService and TankService

---
+++ TankService/src/main/java/com/DragonFish/aquaManage/TankService/TankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.DragonFish.aquaManage.TankService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TankServiceApplication.class, args);
+	}
+
+}
---
+++ TankService/src/test/java/com/DragonFish/aquaManage/TankService/TankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DragonFish.aquaManage.TankService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
