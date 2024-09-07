6b5a04752fe6ecec5395c84b7b0a2c78edeaaeed
Riju Mondal <mailrijumondal@gmail.com>
2024-09-07 19:38:59 +0530

    safe point 1

--- UserService/src/main/java/com/DragonFish/aquaManage/UserService/UserServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.DragonFish.aquaManage.UserService;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.DragonFish.aquaManage.UserService.User;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserServiceRepository extends JpaRepository<User, Long> {
-}
